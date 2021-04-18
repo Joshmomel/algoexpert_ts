5447481bd0dca7f41222402f0637d0024e62ef05
Josh Mo <50355313+Joshmomel@users.noreply.github.com>
2021-04-19 08:47:36 +1000

    chore: remove initial index content

--- index.ts
+++ index.ts
@@ -1,24 +1,4 @@
-export function riverSizes(matrix: number[][]) {
-  // rite your code here.
-  if (matrix.length < 1) return [-1];
-
-  let visited = new Array(matrix.length)
-    .fill(false)
-    .map(() => new Array(matrix[0].length).fill(false));
-  let rivers = [];
-  for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < matrix[0].length; j++) {
-      if (visited[i][j]) continue;
-      if (matrix[i][j] === 0) continue;
-      visited[i][j] = true;
-      // TODO: get river size
-    }
-  }
-
-  console.log('matrix ', matrix);
-  console.log(visited);
-
-  return [-1];
-}
-
-riverSizes(new Array(5).fill(0).map(() => new Array(4).fill(0)));
+/**
+ * Please paste the solution here and run
+ * ts-node index.ts
+ */
