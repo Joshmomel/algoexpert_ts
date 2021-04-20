7d42b80c4b4262972662eec8a720ec5888657b86
Josh Mo <50355313+Joshmomel@users.noreply.github.com>
2021-04-20 22:02:23 +1000

    feat: add firstNonRepeatingCharacter.ts

---
+++ src/easy/firstNonRepeatingCharacter.ts
@@ -0,0 +1,18 @@
+export function firstNonRepeatingCharacter(string: string) {
+  // Write your code here.
+
+  let d1: { [key: string]: number } = {};
+  let charList = string.split('')
+
+  charList.forEach((item) => (d1[item] ? (d1[item] += 1) : (d1[item] = 1)));
+
+  let i = 0;
+  for (const [key, value] of Object.entries(d1)) {
+    if (value === 1) return charList.indexOf(key);
+    i++;
+  }
+
+  return -1;
+}
+
+console.log(firstNonRepeatingCharacter('faadabcbbebdf'));
