3a4d26cad86a30488e93fa20223b999a58e4d0b0
Josh Mo <50355313+Joshmomel@users.noreply.github.com>
2021-05-08 15:14:15 +1000

    refactor: format the code

--- src/medium/longestPalindromicSubstring.ts
+++ src/medium/longestPalindromicSubstring.ts
@@ -1,19 +1,18 @@
 export function longestPalindromicSubstring(string: string) {
   let palindrom = '';
-  if (string.length < 2) return string
+  if (string.length < 2) return string;
   for (let i = 0; i <= string.length; i++) {
     let j = 1;
-    while (i-j>=0 && string.charAt(i - j) === string.charAt(i + j)) {
+    while (i - j >= 0 && string.charAt(i - j) === string.charAt(i + j)) {
       j++;
     }
-    if (j+j > palindrom.length) palindrom = string.substring(i-j+1, i+j);
+    if (j + j > palindrom.length) palindrom = string.substring(i - j + 1, i + j);
 
     j = 1;
-    while (i-j>=0 && string.charAt(i - j) === string.charAt(i + j -1)) {
+    while (i - j >= 0 && string.charAt(i - j) === string.charAt(i + j - 1)) {
       j++;
     }
-    if (j+j > palindrom.length) palindrom = string.substring(i-j +1 , i+j-1);
-
+    if (j + j > palindrom.length) palindrom = string.substring(i - j + 1, i + j - 1);
   }
 
   return palindrom;
